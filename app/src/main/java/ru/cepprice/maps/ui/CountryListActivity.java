334231fce10b6de933d063e87c461039bb5d7ce4
cepprice <super.cepprice@gmail.com>
2020-12-11 12:31:08 +0300

    Initial commit

---
+++ app/src/main/java/ru/cepprice/maps/ui/CountryListActivity.java
@@ -0,0 +1,16 @@
+package ru.cepprice.maps.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.cepprice.maps.R;
+
+import android.os.Bundle;
+
+public class CountryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+    }
+
+}
