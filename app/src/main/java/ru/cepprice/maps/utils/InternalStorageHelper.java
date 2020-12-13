8ad2155b6cc738ac77196e813e46428a5d64c2d2
cepprice <super.cepprice@gmail.com>
2020-12-13 20:21:57 +0300

    Complement helper to read and write to files

--- app/src/main/java/ru/cepprice/maps/utils/InternalStorageHelper.java
+++ app/src/main/java/ru/cepprice/maps/utils/InternalStorageHelper.java
@@ -1,14 +1,48 @@
 package ru.cepprice.maps.utils;
 
+import android.content.Context;
 import android.os.Environment;
 import android.os.StatFs;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class InternalStorageHelper {
 
+    private static final String MAPS_FILE_NAME = "downloaded_maps.txt";
+
     private static File path = Environment.getDataDirectory();
     private static StatFs stat = new StatFs(path.getPath());
+
+    public static void addDownloadedMap(Context context, String mapName) {
+        try {
+            FileOutputStream out = context.openFileOutput(MAPS_FILE_NAME, Context.MODE_APPEND);
+            out.write((mapName + "\n").getBytes());
+            out.flush();
+            out.close();
+        } catch (Exception ignored) {}
+    }
+
+    public static ArrayList<String> getDownloadedMaps(Context context) {
+        ArrayList<String> downloadedMaps = new ArrayList<>();
+
+        try {
+            FileInputStream out = context.openFileInput(MAPS_FILE_NAME);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(out));
+
+            String line = reader.readLine();
+            while (line != null) {
+                downloadedMaps.add(line);
+                line = reader.readLine();
+            }
+        } catch (Exception ignored) {}
+
+        return downloadedMaps;
+    }
 
     public static double getFreeGigabytes() {
         return fromBytesToGigabytes(stat.getAvailableBytes());
